7a6d74292e07dc3d1da508bb3455ffd35387b026
Rithik Castelino <rec66@a1130u05n01.mghpcc.ycrc.yale.edu>
2026-03-12 13:36:45 -0400

    Bug fixes for af_analyze.sh

--- af_analyze.sh
+++ af_analyze.sh
@@ -9,11 +9,11 @@ find ./AlphaScreen_Predictions  -name "*summary_confidences.json" | cut -d '/' -
 
 while read names; do
 for i in {0..4}; do
-for bait_i in {1..$num_baits}; do
+for ((bait_i=1; bait_i<=$num_baits; bait_i++)); do
 ipTM_pos=$((6+(4*$num_baits)+($num_baits**2)+bait_i))
 pae_min_pos=$((11+8*$num_baits+2*($num_baits**2)+bait_i))
 ipTM_bait_i=$(head -$ipTM_pos ./AlphaScreen_Predictions/$names/af_output/inference_output/$names/seed-1_sample-$i/${names}_seed-1_sample-${i}_summary_confidences.json | tail -1 | grep -E -o '[0-9]+\.[0-9]{1,2}')
-pae_min_bait_i=$(head -$pae_min_pos ./AlphaScreen_Predictions/$names/af_output/inference_output/$names    /seed-1_sample-$i/${names}_seed-1_sample-${i}_summary_confidences.json | tail -1 | grep -E -o '[0-9]+\.[0-9]{1,2}')
+pae_min_bait_i=$(head -$pae_min_pos ./AlphaScreen_Predictions/$names/af_output/inference_output/$names/seed-1_sample-$i/${names}_seed-1_sample-${i}_summary_confidences.json | tail -1 | grep -E -o '[0-9]+\.[0-9]{1,2}')
 
 echo "$pae_min_bait_i" >> Output/pae_mins.txt
 echo "$ipTM_bait_i" >> Output/ipTMs.txt 
@@ -23,7 +23,7 @@ done
 done
 done < "Output/complete_accessions.txt"
 
-paste -d', ' Output/pae_mins.txt Output/ipTMs.txt Output/accessions.txt baitindices.txt > Output/statistics.csv
+paste -d', ' Output/pae_mins.txt Output/ipTMs.txt Output/accessions.txt Output/baitindices.txt > Output/statistics.csv
 rm Output/pae_mins.txt
 rm Output/ipTMs.txt
 rm Output/accessions.txt
