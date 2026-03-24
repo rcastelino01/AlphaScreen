c18d4c5f86d4f39c3cdd0701c48782b41bbea60e
Rithik Castelino <rec66@login1.bouchet.ycrc.yale.edu>
2026-03-23 23:31:32 -0400

    Updating slurm, run parameters to 40/20 concurrent cpus/gpus and afterany dependency logic

--- af_run.sh
+++ af_run.sh
@@ -3,8 +3,8 @@
 jobtotal=$(ls -l AlphaScreen_Predictions/ | wc -l) 
 echo $jobtotal
 
-ARRAY_A_ID=$(sbatch --parsable --array=1-${jobtotal}%20 utilities/af_cpu.sh)
+ARRAY_A_ID=$(sbatch --parsable --array=1-${jobtotal}%40 utilities/af_cpu.sh)
 echo "Submitted CPU jobs with ID: $ARRAY_A_ID"
 
-sbatch --dependency=aftercorr:$ARRAY_A_ID --array=1-${jobtotal}%10 utilities/af_gpu.sh
+sbatch --dependency=afterany:$ARRAY_A_ID --array=1-${jobtotal}%20 utilities/af_gpu.sh
 echo "Submitted GPU jobs with dependency on CPU jobs (ID: $ARRAY_A_ID)"
